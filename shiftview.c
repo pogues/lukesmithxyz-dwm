03c9406bb54a83e54b92434d2c9eb2612261da3c
Edward Paul Roe <ep@roe.me>
2020-05-31 12:02:11 +0100

    removed tabs, set rofi on control-space, use alacritty

--- shiftview.c
+++ shiftview.c
@@ -6,63 +6,63 @@
 void
 shiftview(const Arg *arg)
 {
-	Arg a;
-	Client *c;
-	unsigned visible = 0;
-	int i = arg->i;
-	int count = 0;
-	int nextseltags, curseltags = selmon->tagset[selmon->seltags];
+    Arg a;
+    Client *c;
+    unsigned visible = 0;
+    int i = arg->i;
+    int count = 0;
+    int nextseltags, curseltags = selmon->tagset[selmon->seltags];
 
-	do {
-		if(i > 0) // left circular shift
-			nextseltags = (curseltags << i) | (curseltags >> (LENGTH(tags) - i));
+    do {
+        if(i > 0) // left circular shift
+            nextseltags = (curseltags << i) | (curseltags >> (LENGTH(tags) - i));
 
-		else // right circular shift
-			nextseltags = curseltags >> (- i) | (curseltags << (LENGTH(tags) + i));
+        else // right circular shift
+            nextseltags = curseltags >> (- i) | (curseltags << (LENGTH(tags) + i));
 
                 // Check if tag is visible
-		for (c = selmon->clients; c && !visible; c = c->next)
-			if (nextseltags & c->tags) {
-				visible = 1;
-				break;
-			}
-		i += arg->i;
-	} while (!visible && ++count < 10);
+        for (c = selmon->clients; c && !visible; c = c->next)
+            if (nextseltags & c->tags) {
+                visible = 1;
+                break;
+            }
+        i += arg->i;
+    } while (!visible && ++count < 10);
 
-	if (count < 10) {
-		a.i = nextseltags;
-		view(&a);
-	}
+    if (count < 10) {
+        a.i = nextseltags;
+        view(&a);
+    }
 }
 
 void
 shifttag(const Arg *arg)
 {
-	Arg a;
-	Client *c;
-	unsigned visible = 0;
-	int i = arg->i;
-	int count = 0;
-	int nextseltags, curseltags = selmon->tagset[selmon->seltags];
+    Arg a;
+    Client *c;
+    unsigned visible = 0;
+    int i = arg->i;
+    int count = 0;
+    int nextseltags, curseltags = selmon->tagset[selmon->seltags];
 
-	do {
-		if(i > 0) // left circular shift
-			nextseltags = (curseltags << i) | (curseltags >> (LENGTH(tags) - i));
+    do {
+        if(i > 0) // left circular shift
+            nextseltags = (curseltags << i) | (curseltags >> (LENGTH(tags) - i));
 
-		else // right circular shift
-			nextseltags = curseltags >> (- i) | (curseltags << (LENGTH(tags) + i));
+        else // right circular shift
+            nextseltags = curseltags >> (- i) | (curseltags << (LENGTH(tags) + i));
 
                 // Check if tag is visible
-		for (c = selmon->clients; c && !visible; c = c->next)
-			if (nextseltags & c->tags) {
-				visible = 1;
-				break;
-			}
-		i += arg->i;
-	} while (!visible && ++count < 10);
+        for (c = selmon->clients; c && !visible; c = c->next)
+            if (nextseltags & c->tags) {
+                visible = 1;
+                break;
+            }
+        i += arg->i;
+    } while (!visible && ++count < 10);
 
-	if (count < 10) {
-		a.i = nextseltags;
-		tag(&a);
-	}
+    if (count < 10) {
+        a.i = nextseltags;
+        tag(&a);
+    }
 }
