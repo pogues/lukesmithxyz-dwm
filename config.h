c6ed2fa126c561aaf6bf52098d87a8a83a94fe64
Edward Paul Roe <ep@roe.me>
2020-06-20 22:06:52 +0100

    reduced the keybindings; set ranger to popup

--- config.h
+++ config.h
@@ -3,15 +3,15 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
+static const unsigned int gappih    = 2;        /* horiz inner gap between windows */
+static const unsigned int gappiv    = 2;        /* vert inner gap between windows */
+static const unsigned int gappoh    = 2;        /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 2;        /* vert outer gap between windows and screen edge */
+static const int swallowfloating    = 0;        /* 2 means swallow floating windows by default */
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=14", "JoyPixels:pixelsize=14:antialias=true:autohint=true"  };
+static const char *fonts[]          = { "monospace:size=14" };
 static char dmenufont[]             = "monospace:size=14";
 
 /* NORD Colour scheme */
@@ -36,6 +36,8 @@ static char nord13[]                = "#ebcb8b";
 static char nord14[]                = "#a3be8c";
 static char nord15[]                = "#b48ead";
 
+static char black[]                 = "#000000";
+
 
 enum {
     SchemeNorm,
@@ -48,6 +50,7 @@ enum {
     SchemeGreen,
     SchemePurple,
     SchemeWhite,
+    SchemeLayout,
     NUM_COLOR_SCHEMES,   // make sure this is never over 32 or it will clash with ASCII space!
 }; /* color schemes */
 static const char *colors[][3] = {
@@ -61,20 +64,23 @@ static const char *colors[][3] = {
        [SchemeOrange]   = { nord12, nord0,  nord2 },
        [SchemeYellow]   = { nord13, nord0,  nord2 },
        [SchemeGreen]    = { nord14, nord0,  nord2 },
+       [SchemePurple]   = { nord15, nord0,  nord8 },
        [SchemeWhite]    = { nord5,  nord0,  nord2 },
-       [SchemePurple]   = { nord15, nord0,  nord2 },
+       //[SchemeLayout]   = { nord8,  nord0,  nord2 },
+       [SchemeLayout]   = { nord7,  black, nord9 },
 };
 
 typedef struct {
     const char *name;
     const void *cmd;
 } Sp;
-const char *spcmd1[] = {"st", "-n", "spterm", "-g", "120x34", NULL };
-const char *spcmd2[] = {"st", "-n", "spcalc", "-f", "monospace:size=16", "-g", "50x20", "-e", "bc", "-lq", NULL };
+
+const char *sptermcmd[] = {"st", "-n", "spterm", "-g", "140x54", NULL };
+const char *sprangercmd[] = {"st", "-n", "spranger", "-g", "140x54", "-e", "ranger", NULL };
 static Sp scratchpads[] = {
     /* name          cmd  */
-    {"spterm",      spcmd1},
-    {"spranger",    spcmd2},
+    {"spterm",      sptermcmd},
+    {"spranger",    sprangercmd},
 };
 
 /* tagging */
@@ -90,7 +96,7 @@ static const Rule rules[] = {
     { "St",       NULL,       NULL,               0,            0,           1,         0,        -1 },
     { NULL,       NULL,       "Event Tester",     0,            0,           0,         1,        -1 },
     { NULL,      "spterm",    NULL,               SPTAG(0),     1,           1,         0,        -1 },
-    { NULL,      "spcalc",    NULL,               SPTAG(1),     1,           1,         0,        -1 },
+    { NULL,      "spranger",  NULL,               SPTAG(1),     1,           1,         0,        -1 },
 };
 
 /* layout(s) */
@@ -114,15 +120,23 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #define MODKEY Mod4Mask
-#define TAGKEYS(KEY,TAG) \
+/* these are standard keys for selecting/moving to tags */
+#define TAGKEYS(KEY, TAG) \
     { MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
     { MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
     { MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
     { MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+/* alt tags handles the case where shift key is different in the keymap i.e. 2 on mine */
+#define TAGKEYSALT(KEY, SHIFTKEY, TAG) \
+    { MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+    { MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+    { MODKEY|ShiftMask,             SHIFTKEY, tag,            {.ui = 1 << TAG} }, \
+    { MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+
 #define STACKKEYS(MOD,ACTION) \
     { MOD,    XK_j,    ACTION##stack,    {.i = INC(+1) } }, \
     { MOD,    XK_k,    ACTION##stack,    {.i = INC(-1) } }, \
-    { MOD,  XK_v,   ACTION##stack,  {.i = 0 } }, \
+    { MOD,    XK_v,    ACTION##stack,    {.i = 0 } }, \
     /* { MOD, XK_grave, ACTION##stack, {.i = PREVSEL } }, \ */
     /* { MOD, XK_a,     ACTION##stack, {.i = 1 } }, \ */
     /* { MOD, XK_z,     ACTION##stack, {.i = 2 } }, \ */
@@ -141,101 +155,84 @@ static const char *termcmd[]  = { "st", NULL };
 #include "shiftview.c"
 static Key keys[] = {
     /* modifier                     key        function        argument */
+
+    /* selecting windows */
+    /* stack keys sets jkv to move down/up/to start of stack */
     STACKKEYS(MODKEY,                          focus)
     STACKKEYS(MODKEY|ShiftMask,                push)
-    /* { MODKEY|ShiftMask,        XK_Escape,    spawn,    SHCMD("") }, */
-    { MODKEY,            XK_grave,    spawn,    SHCMD("dmenuunicode") },
-    /* { MODKEY|ShiftMask,        XK_grave,    togglescratch,    SHCMD("") }, */
-    TAGKEYS(            XK_1,        0)
-    TAGKEYS(            XK_2,        1)
-    TAGKEYS(            XK_3,        2)
-    TAGKEYS(            XK_4,        3)
-    TAGKEYS(            XK_5,        4)
-    TAGKEYS(            XK_6,        5)
-    TAGKEYS(            XK_7,        6)
-    TAGKEYS(            XK_8,        7)
-    TAGKEYS(            XK_9,        8)
-    { MODKEY,           XK_0,        view,        {.ui = ~0 } },
-    { MODKEY|ShiftMask, XK_0,        tag,        {.ui = ~0 } },
 
-    { MODKEY|ShiftMask, XK_BackSpace,    quit,    {.i = 1} },
+    /* move the selected window to the master */
+    { MODKEY,           XK_space,      zoom,            {0} },
+    
+    /* selecting tags */
 
-    { MODKEY,           XK_Tab,        view,        {0} },
-    /* { MODKEY|ShiftMask,        XK_Tab,        spawn,        SHCMD("") }, */
-    { MODKEY,           XK_q,        killclient,    {0} },
-    { MODKEY|ShiftMask, XK_q,        quit,        {.i = 0} },
-    { MODKEY,           XK_w,        spawn,        SHCMD("$BROWSER") },
-    { MODKEY,           XK_r,        spawn,        SHCMD("st -e ranger") },
-    { MODKEY|ShiftMask, XK_r,        spawn,        SHCMD("st -e htop") },
-    { MODKEY,           XK_t,        setlayout,    {.v = &layouts[0]} }, /* tile */
-    { MODKEY,           XK_d,        setlayout,    {.v = &layouts[1]} }, /* deck */
-    { MODKEY,           XK_m,        setlayout,    {.v = &layouts[2]} }, /* monicle */
-    { MODKEY|ShiftMask, XK_f,        setlayout,    {.v = &layouts[3]} }, /* floating */
-    { MODKEY,           XK_f,        togglefullscr,    {0} },
-    { MODKEY,           XK_o,        incnmaster,     {.i = +1 } },
-    { MODKEY|ShiftMask, XK_o,        incnmaster,     {.i = -1 } },
-    { MODKEY,           XK_backslash,        view,        {0} },
-    /* { MODKEY|ShiftMask,        XK_backslash,        spawn,        SHCMD("") }, */
+    /* Number keys move to tag, shift-# moves screen to tag,
+     * control-# allows to select multiple tags, control-shift-# unselects a tag */
+    TAGKEYS(            XK_1,                   0)
+    TAGKEYSALT(         XK_2, XK_apostrophe,    1)   /* handle the shift-2 being apostrophe */
+    TAGKEYS(            XK_3,                   2)
+    TAGKEYS(            XK_4,                   3)
+    TAGKEYS(            XK_5,                   4)
+    TAGKEYS(            XK_6,                   5)
+    TAGKEYS(            XK_7,                   6)
+    TAGKEYS(            XK_8,                   7)
+    TAGKEYS(            XK_9,                   8)
 
-    { MODKEY,           XK_a,        togglegaps,    {0} },
-    { MODKEY|ShiftMask, XK_a,        defaultgaps,    {0} },
-    { MODKEY,           XK_s,        togglesticky,    {0} },
-    /* { MODKEY|ShiftMask,        XK_s,        spawn,        SHCMD("") }, */
-    { ControlMask,      XK_space,    spawn,          {.v = roficmd } },
-    { MODKEY,           XK_g,        shiftview,    { .i = -1 } },
-    { MODKEY|ShiftMask, XK_g,        shifttag,    { .i = -1 } },
-    { MODKEY,           XK_h,        setmfact,    {.f = -0.05} },
-    /* J and K are automatically bound above in STACKEYS */
-    { MODKEY,           XK_l,        setmfact,          {.f = +0.05} },
-    { MODKEY,           XK_semicolon,    shiftview,    { .i = 1 } },
-    { MODKEY|ShiftMask, XK_semicolon,    shifttag,    { .i = 1 } },
+    /* ; to go to the next populated tag and comma to go back*/
+    { MODKEY,           XK_semicolon,   shiftview,      { .i = 1 } },
+    { MODKEY|ShiftMask, XK_semicolon,   shifttag,       { .i = 1 } },
+    { MODKEY,           XK_comma,       shiftview,      { .i = -1 } },
+    { MODKEY|ShiftMask, XK_comma,       shifttag,       { .i = -1 } },
 
-    { MODKEY,           XK_apostrophe,    togglescratch,    {.ui = 0} },  /* start a st shell scratch */
-    { MODKEY,           XK_Return,    spawn,        {.v = termcmd } },
+    /* the number 0 key or tab selects all tags */
+    { MODKEY,           XK_0,           view,           {.ui = ~0 } },
+    { MODKEY|ShiftMask, XK_0,           tag,            {.ui = ~0 } },
+    { MODKEY,           XK_Tab,         view,           {0} },
+    /* a puts the selected window on every tag */
+    { MODKEY,           XK_a,           togglesticky,   {0} },
 
-    { MODKEY,           XK_z,        incrgaps,    {.i = +3 } },
-    { MODKEY,           XK_x,        incrgaps,    {.i = -3 } },
-    /* V is automatically bound above in STACKKEYS */
-    { MODKEY,           XK_b,        togglebar,    {0} },
+    /* restart */
+    { MODKEY|ShiftMask, XK_BackSpace,   quit,           {.i = 1} },
+    /* exit window/dwm */
+    { MODKEY,           XK_q,           killclient,     {0} },
+    { MODKEY|ShiftMask, XK_q,           quit,           {.i = 0} },
 
-    /*  monitor keys */
-    { MODKEY,           XK_Left,    focusmon,    {.i = -1 } },
-    { MODKEY|ShiftMask, XK_Left,    tagmon,        {.i = -1 } },
-    { MODKEY,           XK_Right,    focusmon,    {.i = +1 } },
-    { MODKEY|ShiftMask, XK_Right,    tagmon,        {.i = +1 } },
+    /* launchers */
+    { ControlMask,      XK_space,       spawn,          {.v = roficmd } },
+    { MODKEY,           XK_d,           spawn,          {.v = dmenucmd } },
+    { MODKEY,           XK_w,           spawn,          SHCMD("$BROWSER") },
+    { MODKEY,           XK_apostrophe,  togglescratch,  {.ui = 0} },  /* start a st shell scratch */
+    { MODKEY,           XK_r,           togglescratch,  {.ui = 1 } }, /* start a scratch ranger */
+    { MODKEY,           XK_Return,      spawn,          {.v = termcmd } },
 
-    { MODKEY,           XK_Insert,    spawn,        SHCMD("notify-send \"📋 Clipboard contents:\" \"$(xclip -o -selection clipboard)\"") },
+    /* layouts */
+    { MODKEY,           XK_u,           setlayout,      {.v = &layouts[0]} }, /* tile */
+    { MODKEY,           XK_i,           setlayout,      {.v = &layouts[1]} }, /* deck */
+    { MODKEY,           XK_o,           setlayout,      {.v = &layouts[2]} }, /* monicle */
+    { MODKEY,           XK_p,           setlayout,      {.v = &layouts[3]} }, /* floating */
+    { MODKEY,           XK_f,           togglefullscr,  {0} },
+    { MODKEY|ShiftMask, XK_f,           togglefloating, {0} },
+    { MODKEY,           XK_m,           incnmaster,     {.i = +1 } },
+    { MODKEY|ShiftMask, XK_m,           incnmaster,     {.i = -1 } },
 
-    { MODKEY,           XK_space,    zoom,        {0} },
-    { MODKEY|ShiftMask, XK_space,    togglefloating,    {0} },
+    /* gaps */
+    { MODKEY,           XK_g,           togglegaps,     {0} },
+    { MODKEY|ShiftMask, XK_g,           defaultgaps,    {0} },
+    { MODKEY,           XK_z,           incrgaps,       {.i = +3 } },
+    { MODKEY,           XK_x,           incrgaps,       {.i = -3 } },
 
-    { 0,                XK_Print,    spawn,        SHCMD("maim pic-full-$(date '+%y%m%d-%H%M-%S').png") },
-    { ShiftMask,        XK_Print,    spawn,        SHCMD("maimpick") },
-    { MODKEY,           XK_Print,    spawn,        SHCMD("dmenurecord") },
-    { MODKEY|ShiftMask, XK_Print,    spawn,        SHCMD("dmenurecord kill") },
-    { MODKEY,           XK_Delete,    spawn,        SHCMD("dmenurecord kill") },
-    { MODKEY,           XK_Scroll_Lock,    spawn,        SHCMD("killall screenkey || screenkey &") },
+    /* resize the master */
+    { MODKEY,           XK_h,           setmfact,       {.f = -0.05} },
+    { MODKEY,           XK_l,           setmfact,       {.f = +0.05} },
 
-    { 0, XF86XK_AudioMute,        spawn,        SHCMD("pamixer -t; kill -44 $(pidof dwmblocks)") },
-    { 0, XF86XK_AudioRaiseVolume,    spawn,        SHCMD("pamixer --allow-boost -i 3; kill -44 $(pidof dwmblocks)") },
-    { 0, XF86XK_AudioLowerVolume,    spawn,        SHCMD("pamixer --allow-boost -d 3; kill -44 $(pidof dwmblocks)") },
-    { 0, XF86XK_AudioPrev,        spawn,        SHCMD("mpc prev") },
-    { 0, XF86XK_AudioNext,        spawn,        SHCMD("mpc next") },
-    { 0, XF86XK_AudioPause,        spawn,        SHCMD("mpc pause") },
-    { 0, XF86XK_AudioPlay,        spawn,        SHCMD("mpc play") },
-    { 0, XF86XK_AudioStop,        spawn,        SHCMD("mpc stop") },
-    { 0, XF86XK_AudioRewind,    spawn,        SHCMD("mpc seek -10") },
-    { 0, XF86XK_AudioForward,    spawn,        SHCMD("mpc seek +10") },
-    { 0, XF86XK_AudioMedia,        spawn,        SHCMD("st -e ncmpcpp") },
-    { 0, XF86XK_PowerOff,        spawn,        SHCMD("sysact") },
-    { 0, XF86XK_Calculator,        spawn,        SHCMD("st -e bc -l") },
-    { 0, XF86XK_Sleep,        spawn,        SHCMD("sudo -A zzz") },
-    { 0, XF86XK_WWW,        spawn,        SHCMD("$BROWSER") },
-    { 0, XF86XK_DOS,        spawn,        SHCMD("st") },
-    { 0, XF86XK_ScreenSaver,    spawn,        SHCMD("slock & xset dpms force off; mpc pause; pauseallmpv") },
-    { 0, XF86XK_TaskPane,        spawn,        SHCMD("st -e htop") },
-    { 0, XF86XK_MonBrightnessUp,    spawn,        SHCMD("xbacklight -inc 15") },
-    { 0, XF86XK_MonBrightnessDown,    spawn,        SHCMD("xbacklight -dec 15") },
+    /* toggle the bar at the top */
+    { MODKEY,           XK_b,           togglebar,      {0} },
+
+    /*  monitor keys  - only have 2 so rotate in one direction */
+    { MODKEY,           XK_s,       focusmon,           {.i = -1 } },
+    { MODKEY|ShiftMask, XK_s,       tagmon,             {.i = -1 } },
+
+    { MODKEY,           XK_Insert,     spawn,           SHCMD("notify-send \"📋 Clipboard contents:\" \"$(xclip -o -selection clipboard)\"") },
 
 
 };
@@ -263,5 +260,6 @@ static Button buttons[] = {
     { ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
     { ClkTagBar,            0,              Button4,        shiftview,      {.i = -1} },
     { ClkTagBar,            0,              Button5,        shiftview,      {.i = 1} },
+/*    { ClkLtSymbol,          0,              Button1,        cyclelayout,    {.i = 1} }, */
     { ClkRootWin,           0,              Button2,        togglebar,      {0} },
 };
