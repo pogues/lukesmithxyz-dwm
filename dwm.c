4653b1c0f1522a23db77b072d34543768f203529
Edward Paul Roe <ep@roe.me>
2020-06-20 22:05:59 +0100

    changed the colour of the layout item on the bar

--- dwm.c
+++ dwm.c
@@ -881,7 +881,7 @@ drawbar(Monitor *m)
         x += w;
     }
     w = blw = TEXTW(m->ltsymbol);
-    drw_setscheme(drw, scheme[SchemeNorm]);
+    drw_setscheme(drw, scheme[SchemeLayout]);
     x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
 
     if ((w = m->ww - sw - x) > bh) {
