704b8e09320c33b822c9c9fe0641d02339cf6237
Edward Paul Roe <ep@roe.me>
2020-12-29 16:02:27 +0000

    set Thunderbird to launch on tag 2

--- config.h
+++ config.h
@@ -102,8 +102,9 @@ static const Rule rules[] = {
      *    WM_CLASS(STRING) = instance, class
      *    WM_NAME(STRING) = title
     */
-    /* class    instance      title            tags mask    isfloating   isterminal  noswallow  monitor */
+    /* class      instance      title            tags mask    isfloating   isterminal  noswallow  monitor */
     { "Gimp",        NULL,       NULL,               1 << 8,       0,           0,         0,        -1 },
+    { "Thunderbird", NULL,       NULL,               1 << 2,       0,           0,         0,        -1 },
     { "st-256color", NULL,       NULL,               0,            0,           1,         0,        -1 },
     { NULL,          NULL,       "spalacritty",      SPTAG(2),     0,           1,         0,        -1 },
     { NULL,          NULL,       "Event Tester",     0,            0,           0,         1,        -1 },
